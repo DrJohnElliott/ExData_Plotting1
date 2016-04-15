e2994fae2f1a8ca558ceb76082517794b67f518a
DrJohnElliott <DrJohnElliott.github@gmail.com>
2016-04-15 12:36:35 -0400

    Created function for graphing

--- plot1.R
+++ plot1.R
@@ -1,3 +1,27 @@
 # Class Project
 setwd("C:/Users/John/Desktop/R-Code/Coursera/Exploratory Data Analysis/Week 1/Class Project 1")
-myData <- read.table("household_power_consumption.txt", nrows = 20, header = TRUE)
+
+#Read data into envioroment
+        tempData <- read.table("household_power_consumption.txt", sep= ";", header = TRUE)
+        myData <- rbind(subset(tempData, Date=="1/2/2007"),subset(tempData, Date=="2/2/2007"))
+        
+#Clean up envioroment
+        rm("tempData")
+
+# Transform Data from factor format
+        myData$Global_active_power <- as.numeric(levels(myData$Global_active_power)[myData$Global_active_power])
+
+#function to plot Data
+        myPlot1 <- function(){
+        
+                hist(myData$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+        }
+
+#Plot data
+        myPlot1()
+        
+#Create PNG file
+        png("plot1.png", width = 480, height = 480, units = "px", pointsize = 12)
+        myPlot1()
+        dev.off()
+
