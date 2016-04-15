b74c18a31c55138b45e55421e225bea25181fd38
DrJohnElliott <DrJohnElliott.github@gmail.com>
2016-04-15 13:14:36 -0400

    Comments added

--- plot1.R
+++ plot1.R
@@ -1,5 +1,6 @@
-# Class Project
-setwd("C:/Users/John/Desktop/R-Code/Coursera/Exploratory Data Analysis/Week 1/Class Project 1")
+#       Exploratory Data Analysis
+#       Week 1 Class Project
+#       John Elliott 4/15/2016
 
 #Read data into envioroment
         tempData <- read.table("household_power_consumption.txt", sep= ";", header = TRUE)
